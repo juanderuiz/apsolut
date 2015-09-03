e138c5ad0933e967db50c3522da6421d8aa827e9
juanderuiz <juanderuiz@gmail.com>
2015-09-03 17:09:31 +0200

    Explicitly declared fog region for S3

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -7,7 +7,8 @@ class Photo < ActiveRecord::Base
   :fog_credentials => {
         provider: 'AWS',
         aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
-        aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"]
+        aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+        region: 'eu-west-1'
   },
   fog_directory: ENV["FOG_DIRECTORY"]
 
