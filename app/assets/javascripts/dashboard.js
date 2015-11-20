828ab888d3b08d77a14974b108f3c533f11c6eee
Juande Ruiz <juanderuiz@gmail.com>
2015-11-20 17:53:29 +0100

    Add D3.js library to show quarters total for year and redesign some views

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -7,9 +7,10 @@ $(document).on('ready page:load',function(){
         $(quarters_year[i]).addClass("hidden");
       }
     }
+    start();
 
     //Attach function for onChange event to show Quarters for a certain year
-    //Hide or Show the correct ones
+    //Hide or Show the correct ones MIRAR ESTO BIEN
     $('#current_year').click(function(){
       var newValue = $('#current_year').val();
       var current_quarters_year = $(".user_quarters").children();
@@ -21,7 +22,41 @@ $(document).on('ready page:load',function(){
           $(quarters_year[i]).removeClass("hidden");
         }
       }
+      start();
     });
 
+    //start();
+
 });
+
+function start() {
+      var yearValue = $('#current_year').val();
+      //var quarters_year = $(".user_quarters").children();
+      var totales = document.getElementsByClassName('total_year');
+      var graphs = document.getElementsByClassName('graph');
+      for (j = 0; j < totales.length; j++){
+        var totalString = $(totales[j]).text().split("-");
+        var totalValue = Math.floor(totalString[0]*100/1440);
+        //window.alert(totalValue);
+        var t = radialProgress(graphs[j])
+          .diameter(150)
+          .value(totalValue)
+          .render();
+      }
+}
+
+function start2(){
+  for (i = 0; i < quarters_year.length; i++) {
+        if ($(quarters_year[i]).hasClass(newValue.toString())) {
+          var total_cell = $(quarters_year[i]).children().last();
+          if ($(total_cell).hasClass('total_year')){
+            window.alert($(total_cell).text());
+            var rp1 = radialProgress($(total_cell))
+                .diameter(150)
+                .value(78)
+                .render();
+          }
+        }
+      }
+}
 
