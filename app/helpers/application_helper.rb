9ef0f87ddcfbb8be60a31d8e6f43923dd15638f3
Juande Ruiz <juanderuiz@gmail.com>
2015-11-20 18:19:19 +0100

    Add migration to User to have an Admin, and helper to link to quarters if admin is logged in

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+	def link_or_not(user, quart)
+  	  if current_user.try(:admin?)
+  	    link_to quart.hours, edit_user_quarter_path(user, quart) 
+      else
+        quart.hours
+      end
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151105150103) do
+ActiveRecord::Schema.define(version: 20151120165759) do
 
   create_table "meetings", force: true do |t|
     t.string   "subject"
@@ -46,9 +46,9 @@ ActiveRecord::Schema.define(version: 20151105150103) do
   add_index "quarters", ["user_id"], name: "index_quarters_on_user_id"
 
   create_table "users", force: true do |t|
-    t.string   "DeviseCreateUsers",      default: "", null: false
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "DeviseCreateUsers",      default: "",    null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 20151105150103) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "name"
+    t.boolean  "admin",                  default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
