6ccb9f3489bac194748481fe035d3823ff1e1651
Juande Ruiz <juanderuiz@gmail.com>
2015-11-24 17:44:14 +0100

    Little change to adapt DDBB to PG

--- db/migrate/20151105150103_change_year_in_quarters.rb
+++ db/migrate/20151105150103_change_year_in_quarters.rb
@@ -1,5 +1,6 @@
 class ChangeYearInQuarters < ActiveRecord::Migration
   def change
-  	change_column :quarters, :year, :integer
+  	change_column :quarters, :year, 'integer USING CAST(year AS integer)'
   end
 end
+
