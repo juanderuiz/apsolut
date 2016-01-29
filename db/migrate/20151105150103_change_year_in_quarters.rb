591d71c98c22b089c916acf39a32f2e06a4b8b3a
Juande Ruiz <juanderuiz@gmail.com>
2016-01-29 17:57:56 +0100

    Change 2 migrations to make quarter/year Integer Type

--- db/migrate/20151105150103_change_year_in_quarters.rb
+++ db/migrate/20151105150103_change_year_in_quarters.rb
@@ -1,6 +1,6 @@
 class ChangeYearInQuarters < ActiveRecord::Migration
   def change
-  	change_column :quarters, :year, 'integer USING CAST("year" AS integer)'
+  	#change_column :quarters, :year, 'integer USING CAST("year" AS integer)'
   end
 end
 
