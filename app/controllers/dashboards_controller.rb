56e21703dfa9ce4313186c28d7cf16ab99a5fc69
Juande Ruiz <juanderuiz@gmail.com>
2016-03-29 11:39:14 +0200

    Remove authorize method from ApplicationController

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,5 +1,5 @@
 class DashboardsController < ApplicationController
-  before_action :authorize
+  before_action :authenticate_user!
 
   def show
   	@quarters = Quarter.all.order(:year, :quart).group_by(&:user_id)
@@ -25,5 +25,4 @@ class DashboardsController < ApplicationController
     @quarters_fc = Quarter.where(user_id: @users_fc).order(:year, :quart).group_by(&:user_id)
     @quarters_sc = Quarter.where(user_id: @users_sc).order(:year, :quart).group_by(&:user_id)
   end
-
 end
