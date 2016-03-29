a85cfa0bd5305f4138909eb9583fcf403c065974
Juande Ruiz <juanderuiz@gmail.com>
2016-03-29 15:04:31 +0200

    Authenticate for Dashboard - debug 5 - routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   #devise_for :users
   devise_for :users, :controllers => { registrations: 'registrations' }
 
-  root 'registrations#new', via: :get
+  root 'devise/sessions#new'
 
   resource :dashboard, only: [:show]
 
