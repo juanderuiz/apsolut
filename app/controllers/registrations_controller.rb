b9d5396442ce973a46e67cf466e838c481c8286c
Juande Ruiz <juanderuiz@gmail.com>
2016-03-29 12:44:51 +0200

    Debugging purposes 3

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,17 +1,5 @@
 class RegistrationsController < Devise::RegistrationsController
 
-  def new
-    super
-  end
-
-  def update
-    super
-  end
-
-  def create
-    super
-  end
-
   private
 
   def sign_up_params
