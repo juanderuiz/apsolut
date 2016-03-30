b5f71e72def9c9b296461302981bfdddece3584d
Juande Ruiz <juanderuiz@gmail.com>
2016-03-30 12:37:33 +0200

    Authenticate for Dashboard - debug 15

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,11 +1,7 @@
 class HomesController < ApplicationController
-  #before_filter :authenticate_user!
-  #before_filter :check_user_signed_in
+ before_filter :check_user_signed_in
 
   def show
-  end
-
-  def start
   end
 
   private
